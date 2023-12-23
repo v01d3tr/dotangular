0cb31bd14837fcbca2ce8624551ea402cec5c5f4
v01d3tr <em.rejman@gmail.com>
2023-12-23 19:57:21 +0100

    add account registering feature

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public AccountController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+        if (await CheckIfUserExists(registerDto.Username))
+            return BadRequest("Username already registered!");
+
+        // "using" to release memory after method finishes
+        // hmac == hashing algorithm
+        using var hmac = new HMACSHA512();
+
+        var newUser = new AppUser
+        {
+            Username = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key //randomly generated key when creating hmac class
+        };
+
+        _context.Add(newUser);
+        await _context.SaveChangesAsync();
+
+        return newUser;
+
+    }
+
+    private async Task<bool> CheckIfUserExists(string username)
+    {
+        return await _context.Users.AnyAsync(x => x.Username == username.ToLower());
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,7 +4,6 @@ public class AppUser
 {
     public int Id { get; set; }
     public string Username { get; set; }
-    // why byte[] for password related stuff?
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
 }
